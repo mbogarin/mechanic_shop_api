6b9b8459393598adba86dcb5dc556653652a6cc0
Magali Bogarin <m.bogarin@outlook.com>
2026-07-07 19:30:28 -0400

    added mechanics blueprint with schemas, CRUD routes, and blueprint registration

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from .extensions import ma
 from .models import db
 from .blueprints.customers import customers_bp
+from .blueprints.mechanics import mechanics_bp
 
 # Initializes Flask app & returns it:
 def create_app(config_name):
@@ -14,6 +15,13 @@ def create_app(config_name):
     db.init_app(app)
     
     # Register blueprints:
+    
+    # 1. Customer:
     app.register_blueprint(customers_bp, url_prefix='/customers') # - note: remove url prefix from CRUD operations. 
+    
+    
+    # 2. Mechanic:
+    app.register_blueprint(mechanics_bp, url_prefix="/mechanics")
+    
     
     return app
--- app/blueprints/mechanics/routes.py
+++ app/blueprints/mechanics/routes.py
@@ -1,3 +1,66 @@
+from flask import jsonify, request
+from marshmallow import ValidationError
+from sqlalchemy import select
 
+from .schemas import mechanic_schema, mechanics_schema
+from app.models import Mechanic, db
+from . import mechanics_bp
 
 # Mechanic routes:
+
+# 1. Create new mechanic (POST):
+@mechanics_bp.route("/", methods=["POST"])
+def create_mechanic():
+    try:
+        mechanic_data = mechanic_schema.load(request.json)
+    except ValidationError as e:
+        return jsonify(e.messages), 400
+    
+    new_mechanic = Mechanic(**mechanic_data)
+    db.session.add(new_mechanic)
+    db.session.commit()
+    
+    return mechanic_schema.jsonify(new_mechanic), 201
+
+
+# 2. Retrieve all mechanics (GET):
+@mechanics_bp.route("/", methods=["GET"])
+def get_mechanics():
+    query = select(Mechanic)
+    mechanics = db.session.execute(query).scalars().all()
+    
+    return mechanics_schema.jsonify(mechanics), 200
+
+
+# 3. Update specific mechanic (PUT):
+@mechanics_bp.route("/<int:mechanic_id>", methods=["PUT"])
+def update_mechanic(mechanic_id):
+    mechanic = db.session.get(Mechanic, mechanic_id)
+    
+    if not mechanic:
+        return jsonify({"error": "Mechanic not found."}), 404
+    
+    try:
+        updated_data = mechanic_schema.load(request.json)
+    except ValidationError as e:
+        return jsonify(e.messages), 400
+
+    for key, value in updated_data.items():
+        setattr(mechanic, key, value)
+        
+    db.session.commit()
+    return mechanic_schema.jsonify(mechanic), 200
+
+
+# 4. Delete specific mechanic (DELETE):
+@mechanics_bp.route("/<int:mechanic_id>", methods=["DELETE"])
+def detele_mechanic(mechanic_id):
+    mechanic = db.session.get(Mechanic, mechanic_id)
+    
+    if not mechanic:
+        return jsonify({"error": "Mechanic not found."}), 404
+    
+    db.session.delete(mechanic)
+    db.session.commit()
+    
+    return jsonify({"message":f"Mechanic id: {mechanic_id}, successfully deleted."}), 200
