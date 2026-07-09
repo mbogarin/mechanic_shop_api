45c11bf48fd245540fdad62a3377112164975c04
Magali Bogarin <m.bogarin@outlook.com>
2026-07-08 23:54:18 -0400

    Added token_required authentication for customer routes

--- app/blueprints/customers/routes.py
+++ app/blueprints/customers/routes.py
@@ -5,14 +5,94 @@ from marshmallow import ValidationError
 from sqlalchemy import select
 
 from .schemas import customer_schema, customers_schema, login_schema
-from app.models import Customer, db
+from app.models import Customer, Service_Ticket, db
 from . import customers_bp
+from app.blueprints.service_tickets.schemas import service_tickets_schema
 
-from app.utils.util import encode_token # Token authentication
+from app.utils.util import encode_token, token_required # Token authentication
 
 
 
-# = Login route (Token Authentication):
+# = 1. Create new customer: (POST)
+@customers_bp.route('/', methods=['POST'])
+def create_customer():
+    try:
+        customer_data = customer_schema.load(request.json)
+    except ValidationError as e:
+        return jsonify(e.messages), 400
+    
+    query = select(Customer).where(Customer.email == customer_data["email"])
+    existing_customer = db.session.execute(query).scalars().all()
+    
+    if existing_customer:
+        return jsonify({"message": "Email is already associated with an account."}), 400
+    
+    new_customer = Customer(**customer_data)
+    db.session.add(new_customer)
+    db.session.commit()
+    return customer_schema.jsonify(new_customer), 201
+
+
+# = 2. Retrieve all customers: (GET)
+@customers_bp.route("/", methods=["GET"])
+def get_customers():
+    query = select(Customer)
+    customers = db.session.execute(query).scalars().all()
+    
+    return customers_schema.jsonify(customers), 200
+
+
+# = 3. Retrieve specific customer: (GET)
+@customers_bp.route("/<int:customer_id>", methods=["GET"])
+def get_customer(customer_id):
+    customer = db.session.get(Customer, customer_id)
+    
+    if customer:
+        return customer_schema.jsonify(customer), 200
+    return jsonify({"error": "Customer not found."}), 404
+
+
+# = 4. Update specific customer: (PUT) - Protected route
+@customers_bp.route("/", methods=["PUT"])
+@token_required # authentication wrapper
+def update_customer(customer_id):
+    
+    customer = db.session.get(Customer, customer_id)
+    
+    if not customer:
+        return jsonify({"error": "Customer not found."}), 404
+    
+    try:
+        updated_data = customer_schema.load(request.json)
+    except ValidationError as e:
+        return jsonify(e.messages), 400
+    
+    for key, value in updated_data.items():
+        setattr(customer, key, value)
+    
+    db.session.commit()
+    return customer_schema.jsonify(customer), 200
+
+
+# = 5. Delete specific customer: (DELETE) - Protected route
+@customers_bp.route("/", methods=["DELETE"])
+@token_required # authentication wrapper
+def delete_customer(customer_id):
+    
+    query = select(Customer).where(Customer.id == customer_id)
+    customer = db.session.execute(query).scalars().first()
+    
+    if not customer:
+        return jsonify({"error": "Customer not found."}), 404
+    
+    db.session.delete(customer)
+    db.session.commit()
+    return jsonify({"message": f"Customer id: {customer_id}, successfully deleted."}), 200
+
+
+
+# = Token Authentication Routes:
+# LOGIN: (POST) - Protected route
 @customers_bp.route("/login", methods=["POST"])
 def login():
     
@@ -39,76 +119,15 @@ def login():
         return jsonify(response), 200
     else:
         return jsonify({"message": 'Invalid email or password! Please try again.'}), 401
-
-
-
-# = 1. Create new customer (POST):
-@customers_bp.route('/', methods=['POST'])
-def create_customer():
-    try:
-        customer_data = customer_schema.load(request.json)
-    except ValidationError as e:
-        return jsonify(e.messages), 400
     
-    query = select(Customer).where(Customer.email == customer_data["email"])
-    existing_customer = db.session.execute(query).scalars().all()
+      
+# Get customer's service tickets after Login: (GET) - protected route
+@customers_bp.route("/my-tickets", methods=["GET"])
+@token_required # Bearer token authentication
+def get_my_tickets(customer_id):
+    query = select(Service_Ticket).where(Service_Ticket.customer_id == customer_id)
     
-    if existing_customer:
-        return jsonify({"message": "Email is already associated with an account."}), 400
-    
-    new_customer = Customer(**customer_data)
-    db.session.add(new_customer)
-    db.session.commit()
-    return customer_schema.jsonify(new_customer), 201
+    service_tickets = db.session.execute(query).scalars().all()
+    return service_tickets_schema.jsonify(service_tickets), 200 
 
-
-# = 2. Retrieve all customers (GET):
-@customers_bp.route("/", methods=["GET"])
-def get_customers():
-    query = select(Customer)
-    customers = db.session.execute(query).scalars().all()
-    
-    return customers_schema.jsonify(customers), 200
-
-
-# = 3. Retrieve specific customer (GET):
-@customers_bp.route("/<int:customer_id>", methods=["GET"])
-def get_customer(customer_id):
-    customer = db.session.get(Customer, customer_id)
-    
-    if customer:
-        return customer_schema.jsonify(customer), 200
-    return jsonify({"error": "Customer not found."}), 404
-
-
-# = 4. Update specific customer (PUT):
-@customers_bp.route("/<int:customer_id>", methods=["PUT"])
-def update_customer(customer_id):
-    customer = db.session.get(Customer, customer_id)
-    
-    if not customer:
-        return jsonify({"error": "Customer not found."}), 404
-    
-    try:
-        updated_data = customer_schema.load(request.json)
-    except ValidationError as e:
-        return jsonify(e.messages), 400
-    
-    for key, value in updated_data.items():
-        setattr(customer, key, value)
-    
-    db.session.commit()
-    return customer_schema.jsonify(customer), 200
-
-
-# = 5. Delete specific customer (DELETE):
-@customers_bp.route("/<int:customer_id>", methods=["DELETE"])
-def delete_customer(customer_id):
-    customer = db.session.get(Customer, customer_id)
-    
-    if not customer:
-        return jsonify({"error": "Customer not found."}), 404
-    
-    db.session.delete(customer)
-    db.session.commit()
-    return jsonify({"message": f"Customer id: {customer_id}, successfully deleted."}), 200
+    # Postman Test: Authorization -> Bearer Token -> Paste token from login.
--- app/utils/util.py
+++ app/utils/util.py
@@ -1,13 +1,17 @@
 # Utility Functions:
+from datetime import datetime, timedelta, timezone
+from jose import jwt
+import jose
+
+from flask import request, jsonify
+from functools import wraps
+
 
 # = Token Authentication:
 
-from datetime import datetime, timedelta, timezone
-from jose import jwt
-
-
 SECRET_KEY = "secret key"
 
+# JWT Authentication:
 def encode_token(customer_id):
     payload = {
         "exp": datetime.now(timezone.utc) + timedelta(days=0, hours=1), # set expiration time.
@@ -19,3 +23,39 @@ def encode_token(customer_id):
     token = jwt.encode(payload, SECRET_KEY, algorithm='HS256')
     return token
 
+
+# TOKEN DECORATOR
+def token_required(f):
+    @wraps(f) # function wrapper.
+    
+    # Functionality:
+    def decorated(*args, **kwargs):
+        
+        # Look for token in authorization header:
+        if "Authorization" in request.headers:
+            token = request.headers['Authorization'].split(" ")[1] # access token string.
+            
+            # If token is NOT found:
+            if not token:
+                return jsonify({"message" : "Token is missing!"}), 401
+            
+            # If token is found:
+            try:
+                data = jwt.decode(token, SECRET_KEY, algorithms=["HS256"]) # Decode the token.
+                customer_id = data['sub'] # Fetch customer ID.
+                
+                
+            # Error messages:
+            except jose.exceptions.ExpiredSignatureError:
+                return jsonify({"message" : "Token has expired!"}), 401 # Expired token.
+            
+            except jose.exceptions.JWTError:
+                return jsonify({"message" : "Invalid Token."}), 401 # Invalid token. 
+            
+        
+            return f(customer_id, *args, **kwargs) # return wrapped function. 
+                
+        else:
+            return jsonify({"message" : "You must be logged in to access this."}), 401
+        
+    return decorated
